3239983e814be1b0f8f5e46988c579f65eacb1e9
Stavitskii <pochta100@mail.ru>
2022-09-07 08:17:29 +0300

    00:15:45

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -8,10 +9,19 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.route("/perform_query")
+@app.route("/perform_query", methods=["POST"])
 def perform_query():
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+
+    data = request.json
+    file_name = data["file_name"]
+    if not os.path.exists(os.path.join(DATA_DIR, file_name)):
+        raise BadRequest
+    return "test"
+
+
+if __name__ == "__main__":
+    app.run()
