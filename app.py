ee9ac3a2323b29de9f38508c0a018034e43108be
Stavitskii <pochta100@mail.ru>
2022-09-08 04:10:53 +0300

    all works

--- app.py
+++ app.py
@@ -14,7 +14,12 @@ def do_cmd(cmd, value, data):
         result = list(filter(lambda record: value in record, data))
     elif cmd == 'map':
         col_num = int(value)
-        result = list(map(lambda record: record.split()[col_num], data))
+        if col_num == 0:
+            result = list(map(lambda record: record.split()[col_num], data))
+        elif col_num == 1:
+            result = list(map(lambda record: record.split()[3] + record.split()[4], data))
+        elif col_num == 2:
+            result = list(map(lambda record: " ".join(record.split()[5:]), data))
     elif cmd == 'unique':
         result = list(set(data))
     elif cmd == 'sort':
@@ -43,20 +48,12 @@ def do_query(params):
 
 @app.route("/perform_query", methods=["POST"])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-
     data = request.json
     file_name = data["file_name"]
     if not os.path.exists(os.path.join(DATA_DIR, file_name)):
         raise BadRequest
 
     return jsonify(do_query(data))
-
-
-# app.response_class('Hello', content_type="text/plain")
 
 
 if __name__ == "__main__":
